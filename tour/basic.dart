03979e8fab4078fff54dc9cdc5d8362d9692e066
Flávio Micheletti <famicheletti@indracompany.com>
2019-06-02 23:33:17 -0300

    lets start

---
+++ tour/basic.dart
@@ -0,0 +1,33 @@
+import "package:test/test.dart";
+
+
+void main() {
+
+    // Define a function.
+    retInteger(int aNumber) {
+      return 'The number is $aNumber.';
+    }
+
+    test("Testing the 'basic dart program'", () {
+        expect('The number is 1.', retInteger(1));
+        expect('The number is 10.', retInteger(10));
+        expect('The number is 99.', retInteger(99));
+    });
+
+
+    test("Testing variables", () {
+        var name = "Bob";
+        expect('Bob', name);
+        expect('Bob', name.toString());
+
+        dynamic name2 = "Bob";
+        expect('Bob', name2);
+        expect('Bob', name2.toString());
+
+        String name3 = "Bob";
+        expect('Bob', name3);
+        expect('Bob', name3.toString());
+    });
+
+
+}
