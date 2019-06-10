dbf12ce1d2ff51d1ebaee9347c75319a7dd4a844
Flávio Micheletti <famicheletti@indracompany.com>
2019-06-09 23:29:05 -0300

    hello world

---
+++ tour/hello-world.dart
@@ -0,0 +1,14 @@
+
+//
+// https://dart.dev/guides/language/language-tour#a-basic-dart-program
+//
+
+main() {
+  
+  print("Hello World");
+  
+  for (var i = 0; i < 4; i++) {
+    print('hello $i');
+  }
+
+}
