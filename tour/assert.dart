cc9abdd4be2d56f1d7b63347bdb451156742c257
Flávio Micheletti <famicheletti@indracompany.com>
2019-06-09 23:29:14 -0300

    assert

---
+++ tour/assert.dart
@@ -0,0 +1,19 @@
+
+//
+// https://dart.dev/guides/language/language-tour#assert
+//
+
+main() {
+  var number = 999;
+  assert(number < 100);
+}
+
+// dart --enable-asserts file.dart
+
+
+/**
+ * See to
+ * 
+ * https://news.dartlang.org/2017/02/dart-122-faster-tools-assert-messages.html
+ * 
+ */
